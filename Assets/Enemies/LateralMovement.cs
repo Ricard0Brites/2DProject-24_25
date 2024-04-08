0f35d720806753ca7deaffa90f4b281ea3974968
Ricardo Brites <ricardo.brites.work@gmail.com>
2024-04-08 14:48:52 +0100

    Added Enemy
    Added Enemy;
    Added Autonomous lateral movement Module;
    Added Enemy Facing Direction To the above mentioned module;

---
+++ Assets/Enemies/LateralMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LateralMovement : MonoBehaviour
+{
+	#region Parameters
+		[SerializeField]
+		private float _MovementSpeed = 5.0f;
+		[SerializeField]
+		private string _WallTag = "Barrier";
+	#endregion
+
+	#region States
+	private EMovementDirection _MovementDirection = EMovementDirection.Right;
+	private bool _CanMove = true; 
+	#endregion
+
+	private void ToggleMovementDirection() 
+	{
+		_MovementDirection = _MovementDirection == EMovementDirection.Right ? EMovementDirection.Left : EMovementDirection.Right; 
+	}
+	private float GetDirectionAsFloat()
+	{ 
+		return _MovementDirection == EMovementDirection.Right ? 1.0f : -1.0f;
+	}
+	private void Move()
+	{
+		if(_CanMove)
+			transform.position += new Vector3((_MovementSpeed * GetDirectionAsFloat()) * Time.deltaTime, 0, 0);
+	}
+	private void UpdateDirection()
+	{
+		transform.eulerAngles = new Vector3(transform.eulerAngles.x, _MovementDirection == EMovementDirection.Right ? 0 : 180, transform.eulerAngles.z);
+	}
+
+	void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+	void FixedUpdate()
+	{
+		Move();
+		UpdateDirection();
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject.CompareTag(_WallTag))
+		{
+			ToggleMovementDirection();
+		}
+	}
+}
