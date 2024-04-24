06a19a55d7622994ad7a2e6ec9e5cdbb8b1ac406
Ricardo Brites <ricardo.brites.work@gmail.com>
2024-04-24 19:05:02 +0100

    Fixed Moving Platform Player Attachment

--- Assets/Platforms/Sliding Platform/SlidingPlatform.cs
+++ Assets/Platforms/Sliding Platform/SlidingPlatform.cs
@@ -6,6 +6,8 @@ public class SlidingPlatform : MonoBehaviour
 {
     [SerializeField] private float _TravelUnits = 2.0f, _TravelSpeed = 0.1f, _FreezeTimeAtDirectionChange = 0.5f;
 	[SerializeField] private bool _PauseAtEnds = true, _IsInTogglingMode = false;
+	private Transform _oldPlayerParent = null;
+	private GameObject _playerRef = null;
 	private bool _PauseMovement = false;
 	private float _StartLocation = 0.0f;
 	private void Start()
@@ -14,20 +16,20 @@ public class SlidingPlatform : MonoBehaviour
 		transform.position -= Vector3.right * _TravelUnits;
 		_TravelSpeed *= _IsInTogglingMode ? -1.0f : 1.0f;
 	}
-	private void Update()
-    {
-		if(!_PauseMovement)
+	private void FixedUpdate()
+	{
+		if (!_PauseMovement)
 		{
 			if (Mathf.Abs(transform.position.x - _StartLocation) > _TravelUnits)
 			{
 				_TravelSpeed *= -1.0f;
 				_PauseMovement = _PauseAtEnds;
-				if(!_IsInTogglingMode)
+				if (!_IsInTogglingMode)
 					StartCoroutine(UnfreezePlatform());
 			}
 			transform.position += Vector3.right * _TravelSpeed * Time.deltaTime;
 		}
-    }
+	}
 	private IEnumerator UnfreezePlatform()
 	{
 		yield return new WaitForSeconds(_FreezeTimeAtDirectionChange);
@@ -41,5 +43,33 @@ public class SlidingPlatform : MonoBehaviour
 	public bool IsPlatformMoving()
 	{
 		return !_PauseMovement;
+	}
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.collider.CompareTag("Player"))
+		{
+			AttachPlayerToSelf(collision.collider.gameObject);
+		}
+	}
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		if (collision.collider.CompareTag("Player"))
+		{
+			DetachPlayer();
+		}
+	}
+	private void AttachPlayerToSelf(GameObject InPlayer)
+	{
+		if (!InPlayer)
+			return;
+		_playerRef = InPlayer;
+		_oldPlayerParent = InPlayer.transform.parent;
+		InPlayer.transform.parent = transform;
+	}
+	private void DetachPlayer()
+	{
+		if (!_playerRef)
+			return;
+		_playerRef.transform.parent = _oldPlayerParent;
 	}
 }
