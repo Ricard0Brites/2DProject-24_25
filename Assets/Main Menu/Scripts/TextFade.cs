9e1173263a209d021d1f476340a9c109dc5b4dd7
Ricardo Brites <ricardo.brites.work@gmail.com>
2024-07-10 14:13:10 +0100

    Fixed "Press Any Key To Continue" Speed & Visibility

--- Assets/Main Menu/Scripts/TextFade.cs
+++ Assets/Main Menu/Scripts/TextFade.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 
 public class TextFade : MonoBehaviour
 {
-    public float FadeSpeed = 1f;
+    public float FadeSpeed = 1f, MinAlpha = 0.5f;
     private Text TextComponent;
     
     void Start()
@@ -12,6 +12,6 @@ public class TextFade : MonoBehaviour
     }
     void Update()
     {
-        TextComponent.color = new Color(TextComponent.color.r, TextComponent.color.g, TextComponent.color.b, Mathf.Cos(Time.time * FadeSpeed));
+        TextComponent.color = new Color(TextComponent.color.r, TextComponent.color.g, TextComponent.color.b, Mathf.Cos(Time.time * FadeSpeed) + MinAlpha);
     }
 }
